87ead8a95f62047f138c6d405f17333c4decf779
Aponia <brianmv@uci.edu>
2023-08-15 13:00:05 -0700

    style: cleanup app entry file

--- apps/antalmanac/src/index.tsx
+++ apps/antalmanac/src/index.tsx
@@ -2,22 +2,22 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 
 /**
- * This function runs in the browser to load the React single-page application.
+ * This function runs in the browser to load the single-page React application.
  */
 async function main() {
     if (typeof document === 'undefined' || typeof window === 'undefined') {
         throw new Error('This function must be run in a browser, not in a Node.js environment.');
     }
 
-    const rootId = 'root';
+    const rootElementId = 'root';
 
-    const root = document.getElementById(rootId);
+    const rootElement = document.getElementById(rootElementId);
 
-    if (!root) {
-        throw new Error(`Please create an element with id ${rootId}`);
+    if (!rootElement) {
+        throw new Error(`Please create an element with id ${rootElementId}`);
     }
 
-    createRoot(root).render(<App />);
+    createRoot(rootElement).render(<App />);
 }
 
 main();
